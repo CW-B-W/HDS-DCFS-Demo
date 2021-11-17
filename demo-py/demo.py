7cf1efcbcf6466780c2bc8379108cae290d5902f
CW-B-W <cw.brad.wang@gmail.com>
2021-11-17 08:27:58 +0000

    Update demo.py

--- demo-py/demo.py
+++ demo-py/demo.py
@@ -165,7 +165,7 @@ df_joined.to_csv(tmp_csv_path, index=False, header=False)
 import subprocess
 #phoenix_home = os.environ['PHOENIX_HOME']
 phoenix_home = "/home/brad/phoenix-hbase-2.3-5.1.2-bin"
-cmd = phoenix_home+"/bin/psql.py 192.168.103.151 -t %s %s %s" % (table_name.upper(), tmp_sql_path, tmp_csv_path)
+cmd = phoenix_home+"/bin/psql.py 192.168.103.151 -t \"%s\" %s %s" % (table_name.upper(), tmp_sql_path, tmp_csv_path)
 process = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 stdout, stderr = process.communicate()
 exit_code = process.wait()
